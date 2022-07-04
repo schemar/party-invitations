bb8be6c1e8b5528a47ce44269f24c0eb40d2c6af
Martin Schenck <martin.schenck@futurice.com>
2022-07-04 21:33:09 +0200

    Add tests for GreatCircleDistance and refine
    Rewritten based on the Wikipedia article.

--- src/PartyInvitations/GreatCircleDistance/GreatCircleDistance.ts
+++ src/PartyInvitations/GreatCircleDistance/GreatCircleDistance.ts
@@ -4,6 +4,9 @@ import { Position } from '../Position.js';
 
 const RADIUS_OF_EARTH_IN_KM = 6371;
 
+/**
+ * Use GreatCircleDistance to calculate the distance between two points on a sphere.
+ */
 export class GreatCircleDistance implements GreatCircleDistanceInterface {
   private readonly radiusOfSphereInKm;
 
@@ -11,14 +14,25 @@ export class GreatCircleDistance implements GreatCircleDistanceInterface {
     this.radiusOfSphereInKm = radiusOfSphereInKm;
   }
 
+  /**
+   * Create a new instance of GreatCircleDistance that uses earth's average
+   * radius for distance calculations.
+   */
   public static newEarth(): GreatCircleDistance {
     return new GreatCircleDistance({
       radiusOfSphereInKm: RADIUS_OF_EARTH_IN_KM,
     });
   }
 
-  // TODO: Implement and test distance().
-  // TODO: Document origin.
+  /**
+   * Returns the distance between two points on a sphere.
+   *
+   * Expects given latitudes and longitudes to be given in degrees.
+   * Note that, for earth, the calculation is not 100% precise:
+   * 1. Earth's radius is not the same everywhere.
+   *    This function assumes a perfect sphere when it calculates the distance.
+   * 2. Elevation is not taken into account.
+   */
   public distance({
     positionOne,
     positionTwo,
@@ -26,16 +40,25 @@ export class GreatCircleDistance implements GreatCircleDistanceInterface {
     positionOne: Position;
     positionTwo: Position;
   }): Distance {
-    const p = Math.PI / 180;
-    const a =
-      0.5 -
-      Math.cos((positionTwo.latitude - positionOne.latitude) * p) / 2 +
-      (Math.cos(positionOne.latitude * p) *
-        Math.cos(positionTwo.latitude * p) *
-        (1 - Math.cos((positionTwo.longitude - positionOne.longitude) * p))) /
-        2;
+    // Convert degrees to radiance:
+    const [latitudeOne, longitudeOne, latitudeTwo, longitudeTwo] = [positionOne.latitude, positionOne.longitude, positionTwo.latitude, positionTwo.longitude].map(degrees => degrees * (Math.PI / 180));
 
-    const distanceInKm = 2 * this.radiusOfSphereInKm * Math.asin(Math.sqrt(a));
+    // Computation of distance based on "Great-circle distance".
+    // For more details see "https://en.wikipedia.org/wiki/Great-circle_distance".
+    // If more accuracy is required, consider using the special case of the
+    // Vincenty formula, as described in the Wikipedia article.
+    let h =
+        Math.pow(
+          Math.sin((latitudeTwo - latitudeOne) / 2),
+          2
+        ) +
+          (Math.cos(latitudeOne) * Math.cos(latitudeTwo) * Math.pow(Math.sin((longitudeTwo - longitudeOne)/2), 2))
+
+    // As per the Wikipedia article, make sure that rounding errors do not lead
+    // to an `h` greater than one.
+    h = Math.min(1.0, h);
+
+    const distanceInKm = 2 * this.radiusOfSphereInKm * Math.asin(Math.sqrt(h));
 
     return Distance.newKm({ distance: distanceInKm });
   }
